9c49b8b15a4f39190356d3d1d9354ac9aa2236b0
jaimin25 <jaiminchovatia25123@gmail.com>
2024-03-11 23:21:10 +0530

    fix: prisma client

--- app/_utils/db.ts
+++ app/_utils/db.ts
@@ -1,7 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
 
-export const db = globalForPrisma.prisma || new PrismaClient();
+export const prisma = globalThis.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma = prisma;
+}
--- app/api/auth/sign-up/route.ts
+++ app/api/auth/sign-up/route.ts
@@ -2,13 +2,23 @@ import { NextRequest, NextResponse } from "next/server";
 import { AppwriteException } from "appwrite";
 import { v4 as uuidv4 } from "uuid";
 
-import prisma from "@/app/_utils/db";
+import { prisma } from "@/app/_utils/db";
 import { singUpUser } from "@/store/appwriteService";
 
 export async function POST(request: NextRequest) {
   const { fullname, email, mobileNo, password } = await request.json();
 
   const uuid = uuidv4();
+
+  const userExists = await prisma.user.findUnique({
+    where: {
+      email,
+      mobileNo,
+    },
+  });
+
+  if (userExists)
+    return NextResponse.json({ statusCode: 403, message: "User already exists with email or mobile no.!" });
 
   const res = await singUpUser(uuid, fullname, email, password);
 
