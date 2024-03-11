45b45c6ea5db3cf2128e49df8c0ea96550e24664
jaimin25 <jaiminchovatia25123@gmail.com>
2024-03-11 22:19:00 +0530

    fix: user unique id using uuid

--- app/api/auth/sign-up/route.ts
+++ app/api/auth/sign-up/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { AppwriteException } from "appwrite";
+import { v4 as uuidv4 } from "uuid";
 
 import prisma from "@/app/_utils/db";
 import { singUpUser } from "@/store/appwriteService";
@@ -7,14 +8,16 @@ import { singUpUser } from "@/store/appwriteService";
 export async function POST(request: NextRequest) {
   const { fullname, email, mobileNo, password } = await request.json();
 
-  const res = await singUpUser("", fullname, email, password);
+  const uuid = uuidv4();
+
+  const res = await singUpUser(uuid, fullname, email, password);
 
   if (res instanceof AppwriteException) {
     return NextResponse.json({ statusCode: res.code, message: res.message });
   } else {
     const createUser = await prisma.user.create({
       data: {
-        uid: "z",
+        uid: uuid,
         fullname,
         email,
         emailVerified: false,
