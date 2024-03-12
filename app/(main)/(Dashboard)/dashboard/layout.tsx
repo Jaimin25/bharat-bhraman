5432e6811179584014b07c25c3b6326ae5a535fc
jaimin25 <jaiminchovatia25123@gmail.com>
2024-03-12 20:52:54 +0530

    feat: user dashboard skeleton while fetching details

--- app/(main)/(Dashboard)/dashboard/layout.tsx
+++ app/(main)/(Dashboard)/dashboard/layout.tsx
@@ -3,12 +3,22 @@
 import { useRouter } from "next/navigation";
 
 import { useSession } from "@/app/_providers/session-provider";
+import UserDashboardSkeleton from "@/components/Skeleton/user-dashboard-skeleton";
 import { Box } from "@chakra-ui/react";
 
 export default function RootDashboardLayout({ children }: { children: React.ReactNode }) {
-  const { isAuthSession } = useSession();
+  const { isAuthSession, isFetching } = useSession();
 
   const router = useRouter();
+
+  if (isFetching) {
+    return (
+      <Box className="mt-14 flex h-full justify-center bg-black/5 p-4">
+        <UserDashboardSkeleton />
+      </Box>
+    );
+  }
+
   if (!isAuthSession) return router.push("/auth/signin");
 
   return <Box className="mt-14 flex h-full justify-center bg-black/5 p-4">{children}</Box>;
