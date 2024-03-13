49f797378f07516c61daf7dcf795c6972ad28b7d
jaimin25 <jaiminchovatia25123@gmail.com>
2024-03-13 22:11:39 +0530

    fix: add user to db first then signup

--- app/api/auth/sign-up/route.ts
+++ app/api/auth/sign-up/route.ts
@@ -26,24 +26,25 @@ export async function POST(request: NextRequest) {
   if (userExists)
     return NextResponse.json({ statusCode: 403, message: "User already exists with email or mobile no.!" });
 
+  const createUser = await prisma.user.create({
+    data: {
+      uid: uuid,
+      fullname,
+      email,
+      emailVerified: false,
+      mobileNo: mobileNo,
+    },
+  });
+
+  if (!createUser) {
+    return NextResponse.json({ statusCode: 500, message: "Internal Server Error!" });
+  }
+
   const res = await singUpUser(uuid, fullname, email, password);
 
   if (res instanceof AppwriteException) {
     return NextResponse.json({ statusCode: res.code, message: res.message });
   } else {
-    const createUser = await prisma.user.create({
-      data: {
-        uid: uuid,
-        fullname,
-        email,
-        emailVerified: false,
-        mobileNo: mobileNo,
-      },
-    });
-    if (createUser) {
-      return NextResponse.json({ statusCode: 200, message: "User Created!" });
-    } else {
-      return NextResponse.json({ statusCode: 500, message: "Internal Server Error!" });
-    }
+    return NextResponse.json({ statusCode: 200, message: "User Created!" });
   }
 }
