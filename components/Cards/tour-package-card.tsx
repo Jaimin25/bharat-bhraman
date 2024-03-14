e2f6bb1e7c8038f02de598f9a5ff94dc4e6f4c49
jaimin25 <jaiminchovatia25123@gmail.com>
2024-03-14 21:28:19 +0530

    fix: packages card update

--- components/Cards/tour-package-card.tsx
+++ components/Cards/tour-package-card.tsx
@@ -1,28 +1,84 @@
-import { Box, Card, CardBody, Stack, Text } from "@chakra-ui/react";
+import { FaLocationDot } from "react-icons/fa6";
+import { FaClock } from "react-icons/fa6";
+import { FaIndianRupeeSign } from "react-icons/fa6";
+import { FaHotel } from "react-icons/fa6";
+import { FaBinoculars } from "react-icons/fa6";
+import { FaCar } from "react-icons/fa6";
+import { GiMeal } from "react-icons/gi";
+
+import { Box, Button, Card, CardBody, Divider, Stack, Text } from "@chakra-ui/react";
 
 import { PackagesImageSlider } from "../TourPackages/packages-images-slider";
 
-export default function TourPackageCard() {
+export default function TourPackageCard({ images }: { images: string[] }) {
   return (
     <Card>
-      <CardBody>
+      <CardBody padding="0px">
         <Stack className="h-auto">
           <Box className="aspect-video">
             <PackagesImageSlider
-              images={[
-                "https://upload.wikimedia.org/wikipedia/commons/9/90/Gir_lion-Gir_forest%2Cjunagadh%2Cgujarat%2Cindia.jpeg",
-                "https://upload.wikimedia.org/wikipedia/commons/1/10/Somanath_mandir_%28cropped%29.jpg",
-              ]}
+              images={images}
               overlay={false}
+              className="rounded-t-md"
             >
               {" "}
             </PackagesImageSlider>
           </Box>
-          <Text>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque sequi distinctio mollitia excepturi impedit
-            assumenda velit quod. Nostrum, accusamus nisi porro eos eveniet quas beatae aliquam quos omnis doloribus
-            dolore.
-          </Text>
+          <Stack
+            padding="18px"
+            paddingTop="0px"
+          >
+            <Box>
+              <Text>Tour Package Title</Text>
+            </Box>
+
+            <Box className="flex items-center gap-4 text-sm">
+              <Box className="flex items-center gap-1">
+                <FaLocationDot />
+                <Text>Location</Text>
+              </Box>
+              <Box className="flex items-center gap-1">
+                <FaClock />
+                <Text>4 Nights/5 Days</Text>
+              </Box>
+            </Box>
+            <Divider />
+            <Box className="flex items-center justify-between gap-2 px-5 *:flex *:flex-col *:items-center *:text-sm">
+              <Box>
+                <FaHotel />
+                <Text>Hotel</Text>
+              </Box>
+              <Box>
+                <FaBinoculars />
+                <Text>Sightseeing</Text>
+              </Box>
+              <Box>
+                <FaCar />
+                <Text>Transport</Text>
+              </Box>
+              <Box>
+                <GiMeal />
+                <Text>Meal</Text>
+              </Box>
+            </Box>
+            <Divider />
+            <Box className="flex items-center">
+              <Box className="flex-1">
+                <Text className="text-sm">Starting from</Text>
+                <Box className="flex items-center">
+                  <FaIndianRupeeSign />
+                  <Text className="font-semibold">15000</Text>
+                </Box>
+                <Text className="text-sm">Per person</Text>
+              </Box>
+              <Button
+                colorScheme="green"
+                size="md"
+              >
+                Book
+              </Button>
+            </Box>
+          </Stack>
         </Stack>
       </CardBody>
     </Card>
--- components/TourPackages/packages-list.tsx
+++ components/TourPackages/packages-list.tsx
@@ -5,14 +5,30 @@ import TourPackageCard from "../Cards/tour-package-card";
 export default function PackagesList() {
   return (
     <Box className="grid h-full flex-1 grid-cols-2 gap-4">
-      <TourPackageCard />
-      <TourPackageCard />
-      <TourPackageCard />
-      <TourPackageCard />
-      <TourPackageCard />
-      <TourPackageCard />
-      <TourPackageCard />
-      <TourPackageCard />
+      <TourPackageCard
+        images={[
+          "https://upload.wikimedia.org/wikipedia/commons/9/90/Gir_lion-Gir_forest%2Cjunagadh%2Cgujarat%2Cindia.jpeg",
+          "https://upload.wikimedia.org/wikipedia/commons/1/10/Somanath_mandir_%28cropped%29.jpg",
+        ]}
+      />
+      <TourPackageCard
+        images={[
+          "https://upload.wikimedia.org/wikipedia/commons/1/13/Amber_palace%2C_Jaipur.jpg",
+          "https://upload.wikimedia.org/wikipedia/commons/0/09/Thar_Khuri.jpg",
+        ]}
+      />
+      <TourPackageCard
+        images={[
+          "https://upload.wikimedia.org/wikipedia/commons/a/ae/Temi_tea_garden.jpg",
+          "https://upload.wikimedia.org/wikipedia/commons/d/d1/Gurudongmar.Lake.jpg",
+        ]}
+      />
+      <TourPackageCard
+        images={[
+          "https://upload.wikimedia.org/wikipedia/commons/a/ad/Munnar_hillstation_kerala.jpg",
+          "https://upload.wikimedia.org/wikipedia/commons/c/c0/Athirapally_Waterfalls_after_the_Monsoons.jpg",
+        ]}
+      />
     </Box>
   );
 }
