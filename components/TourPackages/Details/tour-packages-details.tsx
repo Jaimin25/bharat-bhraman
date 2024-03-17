ace1ec9b55c0aaec508518e3d159467a216f00a2
jaimin25 <jaiminchovatia25123@gmail.com>
2024-03-17 10:28:23 +0530

    feat: skeleton while loading in  package details page

--- components/TourPackages/Details/tour-packages-details.tsx
+++ components/TourPackages/Details/tour-packages-details.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import useFetchDetails from "@/app/_hooks/useFetchDetails";
+import BookSectionSkeleton from "@/components/Skeleton/PackageDetails/book-section-skeleton";
+import DetailsSectionSkeleton from "@/components/Skeleton/PackageDetails/details-section-skeleton";
+import ReviewSectionSkeleton from "@/components/Skeleton/PackageDetails/review-section-skeleton";
 import { Box } from "@chakra-ui/react";
 import { Package } from "@prisma/client";
 
@@ -11,7 +14,16 @@ import PackageReviewSection from "./Sections/package-review-section";
 export default function TourPackageDetailsComponent({ pID }: { pID: string }) {
   const { resData, isFetching } = useFetchDetails<Package>("/api/tour_packages/details", pID);
 
-  if (isFetching) return;
+  if (isFetching)
+    return (
+      <Box className="grid h-full w-full grid-cols-1 gap-4 lg:grid-cols-3">
+        <DetailsSectionSkeleton />
+        <Box className="cols-span-1 grid gap-4">
+          <BookSectionSkeleton />
+          <ReviewSectionSkeleton />
+        </Box>
+      </Box>
+    );
 
   return (
     <Box className="grid h-full w-full grid-cols-1 gap-4 lg:grid-cols-3">
