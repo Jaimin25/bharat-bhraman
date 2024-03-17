20ed82aeb418f8e581cf273993c102ba71d0c1a6
jaimin25 <jaiminchovatia25123@gmail.com>
2024-03-17 19:42:13 +0530

    fix: responsive header nav menu

--- components/Header/header.tsx
+++ components/Header/header.tsx
@@ -2,10 +2,12 @@
 import { useEffect, useState } from "react";
 import NextLink from "next/link";
 import { usePathname } from "next/navigation";
+import { IoMdMenu } from "react-icons/io";
 
 import { useSession } from "@/app/_providers/session-provider";
 import { cn } from "@/app/_utils/cn";
 import { Box, Heading, Link, Stack } from "@chakra-ui/react";
+import { Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 
 export default function HeaderSection() {
   const pathname = usePathname();
@@ -49,7 +51,7 @@ export default function HeaderSection() {
             </Link>
           </Heading>
         </Box>
-        <Box>
+        <Box className="hidden lg:block">
           <Stack
             gap="30px"
             direction="row"
@@ -82,6 +84,28 @@ export default function HeaderSection() {
               </Link>
             )}
           </Stack>
+        </Box>
+        <Box className="flex items-center lg:hidden">
+          <Menu>
+            <MenuButton>
+              <IoMdMenu className="h-6 w-6" />
+            </MenuButton>
+            <MenuList>
+              <NextLink href="/tour_packages">
+                <MenuItem color="black">Packages</MenuItem>
+              </NextLink>
+              <NextLink href="">
+                <MenuItem color="black">Contact Us</MenuItem>
+              </NextLink>
+              <MenuItem color="black">
+                {isAuthSession ? (
+                  <NextLink href="/user/dashboard">Dashboard</NextLink>
+                ) : (
+                  <NextLink href="/auth/signin">SignIn</NextLink>
+                )}
+              </MenuItem>
+            </MenuList>
+          </Menu>
         </Box>
       </Box>
     </header>
