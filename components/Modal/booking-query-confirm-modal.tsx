c81eef756a9eb2e3257687adef86594b3606bd98
jaimin25 <jaiminchovatia25123@gmail.com>
2024-03-17 19:04:40 +0530

    feat: confirmation modal for query booking

---
+++ components/Modal/booking-query-confirm-modal.tsx
@@ -0,0 +1,52 @@
+import NextLink from "next/link";
+
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+} from "@chakra-ui/react";
+
+export default function BookingQueryConfirmModal({
+  isOpen,
+  onClose,
+  uuid,
+}: {
+  isOpen: boolean;
+  onClose: () => void;
+  uuid: string;
+}) {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      isCentered
+      size="md"
+      closeOnOverlayClick={false}
+    >
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Booking Query</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Text>
+            Thank you for choosing BharatBhraman for your Holiday Requirement. Your Query Number is{" "}
+            <span className="font-semibold">{uuid}</span>, One of our travel consultant will revert back to you with the
+            best available options with cheapest prices.
+          </Text>
+        </ModalBody>
+
+        <ModalFooter>
+          <NextLink href="/user/dashboard">
+            <Button colorScheme="green">Go to dashboard</Button>
+          </NextLink>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
--- components/TourPackages/Booking/package-booking.tsx
+++ components/TourPackages/Booking/package-booking.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import { AppwriteException } from "appwrite";
 import axios from "axios";
 import { Field, FieldInputProps, Form, Formik, FormikHelpers, FormikProps } from "formik";
@@ -8,6 +9,7 @@ import { v4 as uuidv4 } from "uuid";
 import useFetchDetails from "@/app/_hooks/useFetchDetails";
 import { useSession } from "@/app/_providers/session-provider";
 import { useToast } from "@/app/_providers/toast-provider";
+import BookingQueryConfirmModal from "@/components/Modal/booking-query-confirm-modal";
 import PackageBookingSkeleton from "@/components/Skeleton/package-booking-skeleton";
 import {
   Box,
@@ -24,6 +26,7 @@ import {
   InputLeftAddon,
   Stack,
   Text,
+  useDisclosure,
 } from "@chakra-ui/react";
 import { BookingQuery, Package } from "@prisma/client";
 
@@ -31,6 +34,9 @@ export default function PackageBookingComponent({ pID }: { pID: string }) {
   const { toastError } = useToast();
   const { sessionUser, currentSession } = useSession();
   const { resData, isFetching } = useFetchDetails<Package>("/api/tour_packages/details", pID);
+  const [uuid, setUuid] = useState<string>();
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   if (isFetching) {
     return <PackageBookingSkeleton />;
@@ -39,6 +45,10 @@ export default function PackageBookingComponent({ pID }: { pID: string }) {
   const handleSubmit = async (values: BookingQuery, actions: FormikHelpers<BookingQuery>) => {
     actions.setSubmitting(true);
     try {
+      const uuid = uuidv4();
+      setUuid(uuid);
+
+      values.id = uuid;
       values.uid = sessionUser?.$id as string;
       values.fullname = sessionUser?.name as string;
       values.email = sessionUser?.email as string;
@@ -56,7 +66,7 @@ export default function PackageBookingComponent({ pID }: { pID: string }) {
       actions.setSubmitting(false);
 
       if (res.data.statusCode === 200) {
-        // s
+        onOpen();
       } else {
         toastError("Error", res.data.message);
       }
@@ -74,177 +84,184 @@ export default function PackageBookingComponent({ pID }: { pID: string }) {
   };
 
   return (
-    <Box className="w-full sm:w-2/5">
-      <Card>
-        <Formik
-          initialValues={{
-            id: uuidv4(),
-            pID: pID,
-            packageTitle: resData?.packageTitle as string,
-            uid: "",
-            fullname: "",
-            email: "",
-            mobileNo: "",
-            adults: "2",
-            child: "0",
-            infant: "0",
-            status: "pending",
-            createdAt: new Date(new Date().toISOString()),
-            updatedAt: new Date(new Date().toISOString()),
-          }}
-          onSubmit={handleSubmit}
-        >
-          {(props: FormikProps<BookingQuery>) => (
-            <Form>
-              <CardBody className="space-y-4">
-                <Heading size="md">Query Form</Heading>
-                <Stack gap="20px">
-                  <Box>
-                    <Field name="packageTitle">
-                      {({ field, form }: { field: FieldInputProps<string>; form: FormikProps<BookingQuery> }) => (
-                        <FormControl isInvalid={!!form.errors.packageTitle && (form.touched.packageTitle as boolean)}>
-                          <FormLabel fontWeight="normal">Package Name</FormLabel>
-                          <Input
-                            {...field}
-                            type="text"
-                            value={form.values.packageTitle}
-                            isReadOnly
-                          />
-                          <FormErrorMessage>{form.errors.packageTitle}</FormErrorMessage>
-                        </FormControl>
-                      )}
-                    </Field>
-                  </Box>
-                  <Box className="flex gap-4">
+    <>
+      <Box className="w-full sm:w-2/5">
+        <Card>
+          <Formik
+            initialValues={{
+              id: "",
+              pID: pID,
+              packageTitle: resData?.packageTitle as string,
+              uid: "",
+              fullname: "",
+              email: "",
+              mobileNo: "",
+              adults: "2",
+              child: "0",
+              infant: "0",
+              status: "pending",
+              createdAt: new Date(new Date().toISOString()),
+              updatedAt: new Date(new Date().toISOString()),
+            }}
+            onSubmit={handleSubmit}
+          >
+            {(props: FormikProps<BookingQuery>) => (
+              <Form>
+                <CardBody className="space-y-4">
+                  <Heading size="md">Query Form</Heading>
+                  <Stack gap="20px">
                     <Box>
-                      <Field
-                        name="adults"
-                        validate={validateForm}
-                      >
+                      <Field name="packageTitle">
                         {({ field, form }: { field: FieldInputProps<string>; form: FormikProps<BookingQuery> }) => (
-                          <FormControl isInvalid={!!form.errors.adults && (form.touched.adults as boolean)}>
-                            <FormLabel fontWeight="normal">Adults</FormLabel>
-                            <Input
-                              {...field}
-                              type="number"
-                              value={form.values.adults}
-                              required
-                            />
-                            <FormErrorMessage>{form.errors.adults}</FormErrorMessage>
-                          </FormControl>
-                        )}
-                      </Field>
-                    </Box>
-                    <Box>
-                      <Field
-                        name="child"
-                        validate={validateForm}
-                      >
-                        {({ field, form }: { field: FieldInputProps<string>; form: FormikProps<BookingQuery> }) => (
-                          <FormControl isInvalid={!!form.errors.child && (form.touched.child as boolean)}>
-                            <FormLabel fontWeight="normal">Child</FormLabel>
-                            <Input
-                              {...field}
-                              type="number"
-                              value={form.values.child}
-                              required
-                            />
-                            <FormErrorMessage>{form.errors.child}</FormErrorMessage>
-                          </FormControl>
-                        )}
-                      </Field>
-                    </Box>
-                    <Box>
-                      <Field
-                        name="infant"
-                        validate={validateForm}
-                      >
-                        {({ field, form }: { field: FieldInputProps<string>; form: FormikProps<BookingQuery> }) => (
-                          <FormControl isInvalid={!!form.errors.infant && (form.touched.infant as boolean)}>
-                            <FormLabel fontWeight="normal">Infant</FormLabel>
-                            <Input
-                              {...field}
-                              type="number"
-                              value={form.values.infant}
-                              required
-                            />
-                            <FormErrorMessage>{form.errors.infant}</FormErrorMessage>
-                          </FormControl>
-                        )}
-                      </Field>
-                    </Box>
-                  </Box>
-                  <Divider />
-                  <Box>
-                    <Field name="fullname">
-                      {({ field, form }: { field: FieldInputProps<string>; form: FormikProps<BookingQuery> }) => (
-                        <FormControl>
-                          <FormLabel fontWeight="normal">Full Name</FormLabel>
-                          <Input
-                            {...field}
-                            type="text"
-                            value={sessionUser?.name}
-                            isReadOnly
-                          />
-                          <FormErrorMessage>{form.errors.fullname}</FormErrorMessage>
-                        </FormControl>
-                      )}
-                    </Field>
-                  </Box>
-                  <Box>
-                    <Field name="email">
-                      {({ field, form }: { field: FieldInputProps<string>; form: FormikProps<BookingQuery> }) => (
-                        <FormControl>
-                          <FormLabel fontWeight="normal">Email</FormLabel>
-                          <Input
-                            {...field}
-                            type="text"
-                            value={sessionUser?.email}
-                            isReadOnly
-                          />
-                          <FormErrorMessage>{form.errors.email}</FormErrorMessage>
-                        </FormControl>
-                      )}
-                    </Field>
-                  </Box>
-                  <Box>
-                    <Field name="mobileNo">
-                      {({ field, form }: { field: FieldInputProps<string>; form: FormikProps<BookingQuery> }) => (
-                        <FormControl>
-                          <FormLabel fontWeight="normal">Mobile No.</FormLabel>
-
-                          <InputGroup>
-                            <InputLeftAddon>
-                              <Text>+91</Text>
-                            </InputLeftAddon>
+                          <FormControl isInvalid={!!form.errors.packageTitle && (form.touched.packageTitle as boolean)}>
+                            <FormLabel fontWeight="normal">Package Name</FormLabel>
                             <Input
                               {...field}
                               type="text"
-                              value={sessionUser?.phone}
+                              value={form.values.packageTitle}
                               isReadOnly
                             />
-                          </InputGroup>
-                          <FormErrorMessage>{form.errors.mobileNo}</FormErrorMessage>
-                        </FormControl>
-                      )}
-                    </Field>
-                  </Box>
-                  <Box>
-                    <Button
-                      className="w-full"
-                      colorScheme="green"
-                      isLoading={props.isSubmitting}
-                      type="submit"
-                    >
-                      Submit
-                    </Button>
-                  </Box>
-                </Stack>
-              </CardBody>
-            </Form>
-          )}
-        </Formik>
-      </Card>
-    </Box>
+                            <FormErrorMessage>{form.errors.packageTitle}</FormErrorMessage>
+                          </FormControl>
+                        )}
+                      </Field>
+                    </Box>
+                    <Box className="flex gap-4">
+                      <Box>
+                        <Field
+                          name="adults"
+                          validate={validateForm}
+                        >
+                          {({ field, form }: { field: FieldInputProps<string>; form: FormikProps<BookingQuery> }) => (
+                            <FormControl isInvalid={!!form.errors.adults && (form.touched.adults as boolean)}>
+                              <FormLabel fontWeight="normal">Adults</FormLabel>
+                              <Input
+                                {...field}
+                                type="number"
+                                value={form.values.adults}
+                                required
+                              />
+                              <FormErrorMessage>{form.errors.adults}</FormErrorMessage>
+                            </FormControl>
+                          )}
+                        </Field>
+                      </Box>
+                      <Box>
+                        <Field
+                          name="child"
+                          validate={validateForm}
+                        >
+                          {({ field, form }: { field: FieldInputProps<string>; form: FormikProps<BookingQuery> }) => (
+                            <FormControl isInvalid={!!form.errors.child && (form.touched.child as boolean)}>
+                              <FormLabel fontWeight="normal">Child</FormLabel>
+                              <Input
+                                {...field}
+                                type="number"
+                                value={form.values.child}
+                                required
+                              />
+                              <FormErrorMessage>{form.errors.child}</FormErrorMessage>
+                            </FormControl>
+                          )}
+                        </Field>
+                      </Box>
+                      <Box>
+                        <Field
+                          name="infant"
+                          validate={validateForm}
+                        >
+                          {({ field, form }: { field: FieldInputProps<string>; form: FormikProps<BookingQuery> }) => (
+                            <FormControl isInvalid={!!form.errors.infant && (form.touched.infant as boolean)}>
+                              <FormLabel fontWeight="normal">Infant</FormLabel>
+                              <Input
+                                {...field}
+                                type="number"
+                                value={form.values.infant}
+                                required
+                              />
+                              <FormErrorMessage>{form.errors.infant}</FormErrorMessage>
+                            </FormControl>
+                          )}
+                        </Field>
+                      </Box>
+                    </Box>
+                    <Divider />
+                    <Box>
+                      <Field name="fullname">
+                        {({ field, form }: { field: FieldInputProps<string>; form: FormikProps<BookingQuery> }) => (
+                          <FormControl>
+                            <FormLabel fontWeight="normal">Full Name</FormLabel>
+                            <Input
+                              {...field}
+                              type="text"
+                              value={sessionUser?.name}
+                              isReadOnly
+                            />
+                            <FormErrorMessage>{form.errors.fullname}</FormErrorMessage>
+                          </FormControl>
+                        )}
+                      </Field>
+                    </Box>
+                    <Box>
+                      <Field name="email">
+                        {({ field, form }: { field: FieldInputProps<string>; form: FormikProps<BookingQuery> }) => (
+                          <FormControl>
+                            <FormLabel fontWeight="normal">Email</FormLabel>
+                            <Input
+                              {...field}
+                              type="text"
+                              value={sessionUser?.email}
+                              isReadOnly
+                            />
+                            <FormErrorMessage>{form.errors.email}</FormErrorMessage>
+                          </FormControl>
+                        )}
+                      </Field>
+                    </Box>
+                    <Box>
+                      <Field name="mobileNo">
+                        {({ field, form }: { field: FieldInputProps<string>; form: FormikProps<BookingQuery> }) => (
+                          <FormControl>
+                            <FormLabel fontWeight="normal">Mobile No.</FormLabel>
+
+                            <InputGroup>
+                              <InputLeftAddon>
+                                <Text>+91</Text>
+                              </InputLeftAddon>
+                              <Input
+                                {...field}
+                                type="text"
+                                value={sessionUser?.phone}
+                                isReadOnly
+                              />
+                            </InputGroup>
+                            <FormErrorMessage>{form.errors.mobileNo}</FormErrorMessage>
+                          </FormControl>
+                        )}
+                      </Field>
+                    </Box>
+                    <Box>
+                      <Button
+                        className="w-full"
+                        colorScheme="green"
+                        isLoading={props.isSubmitting}
+                        type="submit"
+                      >
+                        Submit
+                      </Button>
+                    </Box>
+                  </Stack>
+                </CardBody>
+              </Form>
+            )}
+          </Formik>
+        </Card>
+      </Box>
+      <BookingQueryConfirmModal
+        isOpen={isOpen}
+        onClose={onClose}
+        uuid={uuid as string}
+      />
+    </>
   );
 }
